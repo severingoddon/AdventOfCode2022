f8d61fec8dca9a574df521cfd4edaaf7159acd24
severingoddon <severin.goddon@gmail.com>
2022-12-05 23:19:30 +0100

    complete day5

---
+++ aoc/Days/Day5Part1.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+
+namespace Days
+{
+    class Day5Part1
+    {
+        private static Stack[] stacks = new Stack[9];
+        public static void Part1()
+        {
+            buildStacks();
+            foreach (string line in System.IO.File.ReadLines("./Days/inputs/input_5_2.txt")){
+                string move = Regex.Replace(line, "[A-Za-z]", "");
+                string[] splitted = move.Split("  ");
+                moveStacks(int.Parse(splitted[0]), int.Parse(splitted[1]), int.Parse(splitted[2]));
+            }
+
+            String result = "";
+            for(int i = 0; i<stacks.Length; i++){
+                result += stacks[i].Pop();
+            }
+            Console.WriteLine(result);
+        }
+
+        public static void moveStacks(int count, int sourceStack, int targetStack){
+            for(int i = 0; i<count; i++){
+                stacks[targetStack-1].Push(stacks[sourceStack-1].Pop());
+            }
+        }
+
+        public static void buildStacks() {
+            for(int i = 0; i<9; i++){
+                Stack stack = new Stack();
+                stacks[i] = stack;
+            }
+            foreach (string line in System.IO.File.ReadLines("./Days/inputs/input_5.txt")){
+                int count = 1;
+                for(int i = 0; i<line.Length; i++){
+                    if(i%4==1){
+                        if(Char.ToString(line[i])!=" "){
+                            stacks[count-1].Push(Char.ToString(line[i]));
+                        }
+                        count++;
+                    }
+                }
+                if(line == ""){
+                    break;
+                }
+            }
+            for(int i = 0; i<stacks.Length; i++){
+                stacks[i] = Reverse(stacks[i]);
+            }
+        }
+
+        public static Stack Reverse(Stack input)
+        {
+            Stack temp = new Stack();
+            while (input.Count != 0){
+                temp.Push(input.Pop());
+            }
+            return temp;
+        }
+    }
+}
--- aoc/program.cs
+++ aoc/program.cs
@@ -11,8 +11,13 @@ namespace Days
            //Day1Part2.Part2();
            //Day2Part1.Part1();
            //Day2Part2.Part2();
-           Day3Part1.Part1();
-           Day3Part2_.Part2();
+           //Day3Part1.Part1();
+           //Day3Part2_.Part2();
+           //Day4Part1.Part1();
+           //Day4Part2.Part2();
+           //Day5Part1.Part1();
+           Day5Part2.Part2();
+
         }
     }
 }
